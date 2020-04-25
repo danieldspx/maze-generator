6580d65c49c23d0a36a111f2a08a74efc21638b0
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-04-25 01:26:43 -0300

    Checkpoint

--- Maze.hs
+++ Maze.hs
@@ -6,11 +6,13 @@ module Maze (
 
 import Svg
 import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 
 type Group = Int
 data Cell = AbsentCell | Cell Int Int deriving (Show, Eq, Ord) -- X Y
-data CellProp = CellProp {group::Group, top::Cell, right::Cell, bottom::Cell, left::Cell} deriving (Show)
+data CellProp = AbsentProp | CellProp {group::Group, top::Cell, right::Cell, bottom::Cell, left::Cell} deriving (Show, Ord)
 -- A CellProp with `group = 0` means that it has no group. A group must be >= 1.
+-- Neighbour of a Cell means connection (Wrong name but I might refactor it later, or not)
 
 instance Eq CellProp where
     cellP1 == cellP2 = (group cellP1) == (group cellP2) -- CellProp are equal when their group are.
@@ -20,6 +22,10 @@ divMod' x = snd . divMod x
 
 deleteNth :: Int -> [a] -> [a]
 deleteNth i items = take i items ++ drop (1 + i) items
+
+lookupProp :: Cell -> Map.Map Cell CellProp -> CellProp
+-- lookupProp x y = Map.findWithDefault AbsentProp x y
+lookupProp x y = Maybe.fromJust $ Map.lookup x y
 
 removeDuplicates :: (Foldable t, Eq a, Num a) => t a -> [a]
 removeDuplicates = foldr (\x seen -> if x `elem` seen then seen else x : seen) []
@@ -59,7 +65,7 @@ getEmptyCellProp :: CellProp
 getEmptyCellProp = CellProp {group=0,top=AbsentCell,right=AbsentCell,bottom=AbsentCell,left=AbsentCell}
 
 getNeighbours :: Int -> Cell -> CellProp
-getNeighbours _ AbsentCell = CellProp {group=0,top=AbsentCell,right=AbsentCell,bottom=AbsentCell,left=AbsentCell}
+getNeighbours _ AbsentCell = getEmptyCellProp
 getNeighbours size cell = CellProp {group=0,top=(getTopNeighbour cell),
                                     right=(getRightNeighbour size cell),
                                     bottom=(getBottomNeighbour size cell),
@@ -91,18 +97,66 @@ getMapCells size = Map.fromList tuplesCellNeighbour
 getMapCellsWithGroup :: Int -> Int -> Map.Map Cell CellProp
 getMapCellsWithGroup size startGroup = setGroupMapCells startGroup $ getMapCells size
 
+getConnection :: Int -> CellProp -> Cell
+getConnection pos cellProp = case (pos `divMod'` 4) of 0 -> top cellProp
+                                                       1 -> right cellProp
+                                                       2 -> bottom cellProp
+                                                       3 -> left cellProp
 
-getNeighbour :: Int -> CellProp -> Cell
-getNeighbour randNum cellProp = case (randNum `divMod'` 4) of 0 -> top cellProp
-                                                              1 -> right cellProp
-                                                              2 -> bottom cellProp
-                                                              3 -> left cellProp
+getPossibleConnDirection :: Int -> Cell -> [Int]
+getPossibleConnDirection size cell = map fst $ filter (\x -> (snd x) /= AbsentCell) $ [(0, getTopNeighbour cell),
+                                                                                        (1, getRightNeighbour size cell),
+                                                                                        (2, getBottomNeighbour size cell),
+                                                                                        (3, getLeftNeighbour cell)]
+
+getExistingConnections :: CellProp -> [Cell]
+getExistingConnections cellProp = filter (/=AbsentCell) $ [top cellProp,
+                                                            right cellProp,
+                                                            bottom cellProp,
+                                                            left cellProp]
+
+getAbsentConnection :: CellProp -> [Int] -> [(Int, Cell)]
+getAbsentConnection cellProp possibleConn = filter (\x -> (snd x) == AbsentCell) $ map (\x -> (x, getConnection x cellProp)) possibleConn
 
 shuffleList :: [Int] -> [a] -> [a]
 shuffleList _ (x:[]) = [x]
 shuffleList (n:[]) list = (list!!randPos):(deleteNth randPos list) where randPos = n `divMod'` (length list)
 shuffleList (n:nums) list = (list!!randPos):(shuffleList nums listDel) where listDel = deleteNth randPos list
-                                                                              randPos = n `divMod'` (length list)
+                                                                             randPos = n `divMod'` (length list)
+
+canConnect :: CellProp -> CellProp -> Bool
+canConnect AbsentProp _ = False 
+canConnect _ AbsentProp = False
+canConnect cellProp1 cellProp2 = cellProp1 /= cellProp2 -- If the groups are differents
+
+searchPossibleConnection :: Int -> Map.Map Cell CellProp -> Cell -> [(Int, Cell)]
+searchPossibleConnection size cellsMap cell = filter (\x -> canConnect cellProp $ lookupProp (snd x ) cellsMap) absentNbr where absentNbr = getAbsentConnection cellProp possibleConn
+                                                                                                                                possibleConn = getPossibleConnDirection size cell
+                                                                                                                                cellProp = lookupProp cell cellsMap
+
+stablishConnection :: Cell -> Map.Map Cell CellProp  -> (Int, Cell) -> Map.Map Cell CellProp
+stablishConnection cell cellsMap (connDirect, targetCell) = let cellProp = lookupProp cell cellsMap 
+                                                            in case connDirect of 0 -> Map.insert cell (cellProp {top = targetCell}) cellsMap
+                                                                                  1 -> Map.insert cell (cellProp {right = targetCell}) cellsMap
+                                                                                  2 -> Map.insert cell (cellProp {bottom = targetCell}) cellsMap
+                                                                                  3 -> Map.insert cell (cellProp {left = targetCell}) cellsMap
+
+generateMaze :: Int -> [(Int, Cell)] -> Map.Map Cell CellProp -> Map.Map Cell CellProp
+generateMaze _ [] cellsMap = cellsMap
+generateMaze  size (c:cells) cellsMap = generateMaze size cells updatedCellsMap where cellProp = lookupProp thisCell cellsMap
+                                                                                      updatedCellsMap = if (length possibleConn) /= 0 then stablishConnection thisCell cellsMap (possibleConn!!0) else cellsMap
+                                                                                      possibleConn = searchPossibleConnection size cellsMap thisCell
+                                                                                      thisCell = snd c
+
+-- changeGroupAndPropagate cell cellsMap group = if length connections /= 0 then propagateChildrenGroupChange group connections cellsMap else updatedCellsMap
+--                                                 where updatedCellsMap = Map.insert cell newCellProp cellsCord 
+--                                                       connections = getExistingConnections newCellProp
+--                                                       newCellProp = setGroupCellProp group oldCellProp 
+--                                                       oldCellProp = lookupProp cell cellsMap
+
+-- propagateChildrenGroupChange group (c:[]) cellsMap = Map.toList (changeGroupAndPropagate c cellsMap group)
+-- propagateChildrenGroupChange group (c:cells) cellsMap = (changeGroupAndPropagate c cellsMap group)
+
 
 
                                                 
