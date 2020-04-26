69d723d781bdcf39908e6f66e35dfb72ef524e73
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-04-26 13:54:59 -0300

    Svg creation more general

--- Svg.hs
+++ Svg.hs
@@ -12,20 +12,15 @@ data Dimen = Dimen Int Int -- Width Height
 data Rect = Rect {pos :: Coord, dimen :: Dimen}
 
 -- String inicial do SVG
-svgBegin :: Int -> String
-svgBegin size = printf "<svg width='%d' height='%d' xmlns='http://www.w3.org/2000/svg'>\n" size size
+svgBegin :: Int -> Int -> String
+svgBegin width height = printf "<svg width='%d' height='%d' xmlns='http://www.w3.org/2000/svg'>\n" width height
 
 -- String final do SVG
 svgEnd :: String
 svgEnd = "</svg>"
 
-createRectXML :: Int -> Rect -> String
-createRectXML sizeCell (Rect (Coord x y) (Dimen w h)) = 
-    printf "<rect x='%d' y='%d' width='%d' height='%d' fill='red'/>\n" xReal yReal w h 
-        where
-            xReal = x*sizeCell
-            yReal = y*sizeCell
+createRectXML :: Rect -> String
+createRectXML (Rect (Coord x y) (Dimen w h)) = printf "<rect x='%d' y='%d' width='%d' height='%d' fill='black'/>\n" x y w h
 
 createSvgContent :: Int -> Int -> [Rect] -> String
-createSvgContent sizeSvg sizeCell  walls = (svgBegin $ sizeSvg*sizeCell)++(concat(map (createRectXML sizeCell) walls))++svgEnd
--- (svgBegin $ sizeSvg*sizeCell)++(concat(map (createRectXML sizeCell) walls))++[svgEnd]
+createSvgContent wSvg hSvg  walls = (svgBegin wSvg hSvg)++(concat(map (createRectXML) walls))++svgEnd
