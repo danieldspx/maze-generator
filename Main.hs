bd3a45c03de6dd5435e999cf5781a0322a4c6088
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-04-27 19:50:09 -0300

    Output status of maze cration

--- Main.hs
+++ Main.hs
@@ -4,9 +4,8 @@ import System.IO
 import System.Random
 
 mazeName = "maze.svg"
--- cellList = Maze.getCellList Ex.mazeCoords
-dimenCell = 30
-gridSize = 10
+dimenCell = 10
+gridSize = 50
 thickWall = 10
 
 generateRandomNumbers :: Int -> Int -> Int -> StdGen -> [Int]
@@ -15,5 +14,7 @@ generateRandomNumbers from to total gen = take total $ (randomRs (from,to) gen):
   
 main = do
     gen <- getStdGen
+    putStrLn "Generating maze..."
     writeFile  mazeName $ Maze.generateMazeAndCreateSvg thickWall dimenCell gridSize $ 
         generateRandomNumbers 1 (gridSize^2) (gridSize^2) gen
+    putStrLn "Done."
