bc6dc1f82d26a2ebd5c64d6caae650466f3062ae
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-04-28 16:47:35 -0300

    Main to generate maze and solution

--- Main.hs
+++ Main.hs
@@ -1,20 +1,28 @@
 
-import qualified Maze.Generator as Maze
+import qualified Maze.Generator as Generator
+import qualified Maze.Solver as Solver
 import System.IO
 import System.Random
 
 mazeName = "maze.svg"
+mazeSolvedName = "maze_solution.svg"
 dimenCell = 10
-gridSize = 50
+gridSize = 5
 thickWall = 10
+solutionCellStart = Generator.Cell 0 0
+solutionCellEnd = Generator.Cell (gridSize-1) (gridSize-1)
 
 generateRandomNumbers :: Int -> Int -> Int -> StdGen -> [Int]
 generateRandomNumbers from to total gen = take total $ (randomRs (from,to) gen)::([Int])
 
   
 main = do
-    gen <- getStdGen
-    putStrLn "Generating maze..."
-    writeFile  mazeName $ Maze.generateMazeAndCreateSvg thickWall dimenCell gridSize $ 
-        generateRandomNumbers 1 (gridSize^2) (gridSize^2) gen
-    putStrLn "Done."
+        gen <- getStdGen
+        let cellsMapMaze = Generator.initializeMazeAndGenerate gridSize $ generateRandomNumbers 1 (gridSize^2) (gridSize^2) gen
+        putStrLn "Generating maze..."
+        writeFile mazeName $ Generator.createSvgFromCellsMap thickWall dimenCell gridSize cellsMapMaze
+        putStrLn "Generated."
+        putStrLn "Solving maze..."
+        let cellsMapSolution = Solver.getCellMapForShortestPathSolution cellsMapMaze solutionCellStart solutionCellEnd
+        writeFile mazeSolvedName $ Generator.createSvgFromCellsMap thickWall dimenCell gridSize cellsMapSolution
+        putStrLn "Done."
